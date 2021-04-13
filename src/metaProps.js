0118913de32e3dea3c4b26383ab6be5ec0faf135
Josh Dura <josh.dura@graphiumhealth.com>
2021-04-13 09:57:55 -0500

    Remove propTypes

--- src/metaProps.js
+++ src/metaProps.js
@@ -1,17 +1,4 @@
 import React from 'react';
-import { string, array, oneOfType, bool, func } from 'prop-types';
-
-
-export const propTypes = {
-  data: oneOfType([string, array]).isRequired,
-  headers: array,
-  target: string,
-  separator: string,
-  filename: string,
-  uFEFF: bool,
-  onClick: func,
-  asyncOnClick: bool
-};
 
 export const defaultProps = {
   separator: ',',
